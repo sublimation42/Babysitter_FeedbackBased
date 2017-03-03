b37172ff18094f300b5331f51acf74ab58c187d8
sublimation42 <catchtwotwoallstar@gmail.com>
2017-03-03 09:47:24 -0500

    first test, sitter greeting including creation of Sitter class complete.

---
+++ src/Sitter.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Adam on 3/3/2017.
+ */
+public class Sitter {
+
+
+    public String sitterGreeting(String incoming) {
+        return "Hello I am Sitter2k.";
+    }
+}
--- test/SitterTest.java
+++ test/SitterTest.java
@@ -1,9 +1,20 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Adam Howard on 03/03/2017.
  */
 public class SitterTest {
-    
 
+    @Test
+    public void TestSitterGreeting()
+    {
+
+        Sitter a = new Sitter();
+
+       assertEquals("Hello I am Sitter2k.", a.sitterGreeting("Hello")) ;
+
+    }
 
 
 }
