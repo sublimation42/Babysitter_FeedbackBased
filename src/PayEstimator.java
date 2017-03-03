67ab6f276e8f20fad7dd2f8282163e6408d10990
sublimation42 <catchtwotwoallstar@gmail.com>
2017-03-03 16:59:11 -0500

    static normalRate variable added to replace the directly returned integer of 12.  all passed.

--- src/PayEstimator.java
+++ src/PayEstimator.java
@@ -2,11 +2,14 @@
  * Created by Adam on 3/3/2017.
  */
 public class PayEstimator {
+    public static final int normalRate = 12;
+
+
     public int calculatePay(int hrs) {
         return hrs;
     }
 
     public int calcNormalPay(String start, String bed) {
-        return 12;
+        return normalRate;
     }
 }
